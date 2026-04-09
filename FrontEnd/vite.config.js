3c9123fe033ae1ff4e182ea01c59fd18a716beee
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2026-04-09 17:27:37 +0700

    feat: setup pure GitHub CI/CD pipeline and GitHub Pages config

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Đảm bảo đường dẫn tương đối để chạy được trên GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
