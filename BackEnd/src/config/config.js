299578fbd45fa2c60afb73ff7ed653b0e28e3ed9
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2026-04-21 14:16:00 +0700

    feat(config): update production database config
    - Use environment variables for DB credentials
    - Add DB port configuration
    - Enable SSL for production database connection

--- BackEnd/src/config/config.js
+++ BackEnd/src/config/config.js
@@ -18,10 +18,20 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_PROD_NAME || "project_app_prod",
-    host: process.env.DB_HOST || "127.0.0.1",
-    dialect: process.env.DB_DIALECT || "mysql",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
+    logging: false,
   },
 };
